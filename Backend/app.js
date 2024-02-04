c3aa259cde03163d8635cbcbf40ee91a5b0bde72
CYRIL C KURIAN <cyril.kurian2@gmail.com>
2024-02-04 13:32:24 +0530

    Created connection to MongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const createError = require('http-errors')
 require('dotenv').config()
-
+require('./helpers/inti_mongodb')
 
 const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
+app.use(morgan('dev'))
+
 
 app.get('/',async(req, res, next) => {
 	res.send("Hello from express")
