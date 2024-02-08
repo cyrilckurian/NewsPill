82c8b01ab8a687968da9df991cd983d36fefe397
Abijith-Suresh <abijithsuresh47@gmail.com>
2024-02-08 15:20:50 +0530

    Added event listner for login and signup pages

--- Frontend/news-pill/src/pages/login/LogIn.tsx
+++ Frontend/news-pill/src/pages/login/LogIn.tsx
@@ -1,39 +1,65 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 import NavBar from "../../components/NavBar";
 import "./LogIn.css";
 
 function LogIn() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassowrd] = useState<string>('');
+
+  const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post('https://localhost:3000/auth/login', { email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-    <NavBar />
-    <div className="log-in">
-      <div className="wrapper">
-        <form action="">
-          <h1>Login</h1>
-          <div className="input-box">
-            <input type="text" placeholder="Username" required></input>
-          </div>
-          <div className="input-box">
-            <input type="password" placeholder="Password" required></input>
-          </div>
+      <NavBar />
+      <div className="log-in">
+        <div className="wrapper">
+          <form action="" onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="Username"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(e) => setPassowrd(e.target.value)}
+              ></input>
+            </div>
 
-          <div className="remember-forgot">
-            <label>
-              <input type="checkbox" /> Remember me
-            </label>
-          </div>
-          <button type="submit" className="btn">
-            Login
-          </button>
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+            <button type="submit" className="btn">
+              Login
+            </button>
 
-          <div className="register-link">
-            <p>
-              Don't have an account? <Link to="/signup" className="signup-link">Register</Link>
-            </p>
-          </div>
-        </form>
+            <div className="register-link">
+              <p>
+                Don't have an account?{" "}
+                <Link to="/signup" className="signup-link">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 }
--- Frontend/news-pill/src/pages/signup/SignUp.tsx
+++ Frontend/news-pill/src/pages/signup/SignUp.tsx
@@ -1,20 +1,34 @@
 import "./SignUp.css";
 import NavBar from "../../components/NavBar";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassowrd] = useState<string>('');
+
+  const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post('https://localhost:3000/auth/login', { email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
     <NavBar />
     <div className="signup-in">
       <div className="wrapper">
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <h1>Sign Up</h1>
           <div className="input-box">
-            <input type="text" placeholder="Username" required></input>
+            <input type="text" placeholder="Username" required onChange={(e) => setEmail(e.target.value)}></input>
           </div>
           <div className="input-box">
-            <input type="password" placeholder="Password" required></input>
+            <input type="password" placeholder="Password" required onChange={(e) => setPassowrd(e.target.value)}></input>
           </div>
           <button type="submit" className="btn">
             Sign Up
